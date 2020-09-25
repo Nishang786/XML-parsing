1b04af8cfda2a9b4e0056f866b6510fd9e2497cc
Nishang Lingalwar <nishang786@gmail.com>
2020-09-25 05:44:55 +0100

    Add files via upload

---
+++ urllinks.py
@@ -0,0 +1,10 @@
+from urllib import request
+from bs4 import BeautifulSoup
+
+url = input('Enter -')
+web = request.urlopen(url).read()
+soup = BeautifulSoup(web,'html.parser')
+tags = soup('title')
+for tag in tags:
+      print('title-\n',tag.text)
+      
